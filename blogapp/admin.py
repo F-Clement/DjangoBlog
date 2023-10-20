6f60e77f92b45257b10ded151628e46b9477e1a1
F-Clement <>
2023-10-20 09:22:11 +0000

    set to only text now

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -10,7 +10,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('text', 'content',)
+    summernote_fields = ('text',)
 
 
 admin.site.register(Post, PostAdmin)
