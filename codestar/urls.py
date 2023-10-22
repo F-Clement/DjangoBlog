6a33c931df3b9478ed03a1913e1a96aaefcf8cab
F-Clement <>
2023-10-22 11:16:19 +0000

    urls connected

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blogapp.urls'), name='blog_urls'),
 ]
 
 if settings.DEBUG:
